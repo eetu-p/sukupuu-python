fc8b9094c2455b9a0c9d035072b8444a25bb92d1
Eetu <eetu.poutiainen@live.com>
2026-07-29 16:35:23 +0300

    Lisää Person-luokka

--- database.py
+++ database.py
@@ -5,6 +5,8 @@ database_name = 'Sukupuuohjelma'
 username = 'eetu'
 password = '1234'
 port = 5432
+conn = None
+cur = None
 
 try:
     conn = psycopg2.connect(
@@ -15,6 +17,24 @@ try:
         port = port
     )
 
-    conn.close()
+    cur = conn.cursor()
+
+    create_script = '''
+        CREATE TABLE IF NOT EXISTS person (
+            id      int PRIMARY KEY,
+            given_name    varchar(128) NOT NULL,
+            last_name     varchar(128) NOT NULL,
+            date_of_birth   date,
+            date_of_death   date
+        )
+    '''
+    cur.execute(create_script)
+
+    conn.commit()
 except Exception as error:
     print(error)
+finally:
+    if cur is not None:
+        cur.close()
+    if conn is not None:
+        conn.close()
---
+++ models.py
@@ -0,0 +1,14 @@
+class Person:
+    def __init__(
+            self, 
+            id, 
+            given_name, 
+            last_name, 
+            date_of_birth, 
+            date_of_death
+            ):
+        self.id = id
+        self.given_name = given_name
+        self.last_name = last_name
+        self.date_of_birth = date_of_birth
+        self.date_of_death = date_of_death
