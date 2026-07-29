3df08b258dc9ce36eaad1e90f6a41bb21fe2e02b
Eetu <eetu.poutiainen@live.com>
2026-07-29 21:05:31 +0300

    Luo create_person-funktio

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from connection import get_connection
 
-def create_tables():
+def create_table():
     conn = get_connection()
     cur = conn.cursor()
 
     cur.execute('''
         CREATE TABLE IF NOT EXISTS person (
-            id              INT PRIMARY KEY,
+            id              SMALLSERIAL PRIMARY KEY,
             given_name      VARCHAR(128),
             last_name       VARCHAR(128),
             date_of_birth   DATE,
@@ -17,3 +17,26 @@ def create_tables():
     conn.commit()
     cur.close()
     conn.close()
+
+def create_person(
+        given_name,
+        last_name,
+        date_of_birth,
+        date_of_death
+        ):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute('''
+                    INSERT INTO person (
+                        given_name, 
+                        last_name, 
+                        date_of_birth, 
+                        date_of_death
+                    )
+                    VALUES (%s, %s, %s, %s)
+                    RETURNING id;
+                ''',
+                (given_name, last_name, date_of_birth, date_of_death)
+            )
+
+            return cur.fetchone()[0]
